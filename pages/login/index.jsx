dd93cd7b5b27db003ac203cfb2b52e74140600e6
Siyalov <91375207+Siyalov@users.noreply.github.com>
2022-11-18 01:56:27 +0200

    login scroll

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from 'react';
-import { StyleSheet, View } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
 import { Text, Button } from '@rneui/themed';
 import { InputField } from "../../components/InputField";
 // import InputField
@@ -24,37 +24,39 @@ export default function Form({ navigation }) {
    return (
       <>
          <Text h2 style={{ textAlign: 'center' }}>Чем Вы можете помочь?</Text>
-         <InputField
-            head="Имя/Организация"
-            placeholder="Введите имя"
-            info="Вы должны дать нам понять как вас зовут."
-            onChangeValue={setName}
-            value={name}
-         />
-         <InputField
-            head="Номер телефона"
-            placeholder="+358 0 000 000"
-            info="Как с вами можно связаться?"
-            onChangeValue={setPhone}
-            value={phone}
-         />
-         <InputField
-            head="Чем вы можете помочь беженцам"
-            placeholder="Введите информацию о возможной помощи"
-            info="Расскажите, чем вы можете помочь"
-            onChangeValue={setHelpRequest}
-            value={helpRequest}
-            multiline={true}
-            numberOfLines={4}
-         />
+         <ScrollView>
+            <InputField
+               head="Имя/Организация"
+               placeholder="Введите имя"
+               info="Вы должны дать нам понять как вас зовут."
+               onChangeValue={setName}
+               value={name}
+            />
+            <InputField
+               head="Номер телефона"
+               placeholder="+358 0 000 000"
+               info="Как с вами можно связаться?"
+               onChangeValue={setPhone}
+               value={phone}
+            />
+            <InputField
+               head="Чем вы можете помочь беженцам"
+               placeholder="Введите информацию о возможной помощи"
+               info="Расскажите, чем вы можете помочь"
+               onChangeValue={setHelpRequest}
+               value={helpRequest}
+               multiline={true}
+               numberOfLines={4}
+            />
 
-         {/* <Text>Обратиться за помощью</Text> */}
-         <Button
-            title={"Отправить \nинформацию"}
-            radius={'md'}
-            style={{ padding: 16 }}
-            onPress={onAddClick}
-         />
+            {/* <Text>Обратиться за помощью</Text> */}
+            <Button
+               title={"Отправить \nинформацию"}
+               radius={'md'}
+               style={{ padding: 16 }}
+               onPress={onAddClick}
+            />
+         </ScrollView>
       </>
    );
 }
